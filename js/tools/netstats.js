e2e13d4beebec6077619916dc968cfca614f0554
Purbayan Chowdhury <pur.cho.99@gmail.com>
2020-09-24 23:52:45 +0530

    netstatsv0.0.3

--- js/tools/netstats.js
+++ js/tools/netstats.js
@@ -1,3 +1,13 @@
+function calculateRatio(num_1, num_2) {
+	for (num = num_2; num > 1; num--) {
+		if (num_1 % num == 0 && num_2 % num == 0) {
+			num_1 = num_1 / num;
+			num_2 = num_2 / num;
+		}
+	}
+	var ratio = num_1 + ":" + num_2;
+	return ratio;
+}
 (($) => {
 	let {
 		appCodeName,
@@ -15,21 +25,39 @@
 	let netStat = `
 	<h2>Browser Info:</h2>
 	<ul>
-		<li>CodeName: <span id="bro-cname">${appCodeName}</span></li>
-		<li>Name: <span id="bro-name">${appName}</span></li>
-		<li>Version: <span id="bro-ver">${appVersion}</span></li>
+		<!--<li>CodeName: <span id="bro-cname">${appCodeName}</span></li>-->
+		<li>Name: <span id="bro-name">${$.browser.name}</span></li>
+		<li>Version: <span id="bro-ver">${$.browser.version}</span></li>
 		<li>Platform: <span id="bro-plat">${platform}</li>
-		<li>Cookies Enabled: <span id="bro-cookie">${cookieEnabled}</span></li>
 		<li>Languages: <span id="bro-lang">${language}</span></li>
-		<li>Status: <span id="bro-stat">${onLine}</span></li>
-		<li>User Agent: <span id="usr-agent">${userAgent}</li>
-		
+		<li>User Agent: <span id="usr-agent">${userAgent}</li>		
+	</ul>
+	<h2>Status:</h2>
+	<ul>
+		<li>Is Online?: <span id="bro-stat">${onLine}</span></li>
+		<li>Cookies Enabled: <span id="bro-cookie">${
+			cookieEnabled ? "Yes" : "No"
+		}</span></li>
+		<li>Javascript Enabled: <span>${
+			navigator.javaEnabled() ? "Yes" : "No"
+		}</span></li>
 	</ul>
 	<h2> Window Info: </h2>
 	<ul>
 		<li>
 			Dimensions: <span id="win-width"></span>px x
 			<span id="win-height"></span>px
+		</li>
+		<li>
+			Screen: <span id="scr-width">${screen.width}</span>px x 
+			<span id="scr-height">${screen.height}</span>px
+		</li>
+		<li>
+			Resolution: <span id="scr-res">${calculateRatio(
+				screen.pixelDepth,
+				screen.colorDepth
+			)}</span> 
+			(<span id="scr-depth">${screen.colorDepth}</span>bit)
 		</li>
 	</ul>
 	`;
@@ -40,13 +68,21 @@
 		container = $("#locationinfo");
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function (position) {
-				container.html(
-					`<div class="p-2">
+				$.getJSON("http://gd.geobytes.com/GetCityDetails?callback=?", function (
+					data
+				) {
+					container.html(
+						`<div class="p-2">
 					Latitude: &nbsp;${position.coords.latitude}
-				    <br>Longitude: &nbsp;${position.coords.longitude}
+					<br>Longitude: &nbsp;${position.coords.longitude}
+					<br>
+					Remote IP: &nbsp; ${data.geobytesremoteip}
+					<br>
+					Place: &nbsp; ${data.geobytesfqcn}
 					</div>
 					`
-				);
+					);
+				});
 			});
 		} else {
 			container.html(
@@ -54,26 +90,72 @@
 			);
 		}
 	});
-
-	fetch("http://gd.geobytes.com/GetCityDetails", {
-		headers: {
-			accept:
-				"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-			"accept-language": "en-US,en;q=0.9,bn;q=0.8,hi;q=0.7,la;q=0.6",
-			"cache-control": "max-age=0",
-			"upgrade-insecure-requests": "1",
-		},
-		referrerPolicy: "no-referrer",
-		body: null,
-		method: "GET",
-		mode: "cors",
-		credentials: "omit",
-	}).then((response) => {
-		console.log(response);
-	});
 })(jQuery);
 
 $(window).on("load reload resize", function () {
 	$("#win-width").text($(window).width());
 	$("#win-height").text($(window).height());
+	findLocalIp();
 });
+
+const findLocalIp = (logInfo = true) =>
+	new Promise((resolve, reject) => {
+		window.RTCPeerConnection =
+			window.RTCPeerConnection ||
+			window.mozRTCPeerConnection ||
+			window.webkitRTCPeerConnection;
+
+		if (typeof window.RTCPeerConnection == "undefined")
+			return reject("WebRTC not supported by browser");
+
+		let pc = new RTCPeerConnection();
+		let ips = [];
+
+		pc.createDataChannel("");
+		pc.createOffer()
+			.then((offer) => pc.setLocalDescription(offer))
+			.catch((err) => reject(err));
+		pc.onicecandidate = (event) => {
+			if (!event || !event.candidate) {
+				// All ICE candidates have been sent.
+				if (ips.length == 0)
+					return reject("WebRTC disabled or restricted by browser");
+
+				return resolve(ips);
+			}
+
+			let parts = event.candidate.candidate.split(" ");
+			let [
+				base,
+				componentId,
+				protocol,
+				priority,
+				ip,
+				port,
+				,
+				type,
+				...attr
+			] = parts;
+			let component = ["rtp", "rtpc"];
+
+			if (!ips.some((e) => e == ip)) ips.push(ip);
+
+			if (!logInfo) return;
+
+			console.log(" candidate: " + base.split(":")[1]);
+			console.log(" component: " + component[componentId - 1]);
+			console.log("  protocol: " + protocol);
+			console.log("  priority: " + priority);
+			console.log("        ip: " + ip);
+			console.log("      port: " + port);
+			console.log("      type: " + type);
+
+			if (attr.length) {
+				console.log("attributes: ");
+				for (let i = 0; i < attr.length; i += 2)
+					console.log("> " + attr[i] + ": " + attr[i + 1]);
+			}
+
+			console.log();
+		};
+	});
