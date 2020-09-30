ce4f9cd2077aed99df2e9594db7b117a0f369277
Purbayan Chowdhury <pur.cho.99@gmail.com>
2020-09-30 23:54:42 +0530

    Minor addition

--- js/tools/netstats.js
+++ js/tools/netstats.js
@@ -8,96 +8,6 @@ function calculateRatio(num_1, num_2) {
 	var ratio = num_1 + ":" + num_2;
 	return ratio;
 }
-(($) => {
-	let {
-		appCodeName,
-		appName,
-		appVersion,
-		cookieEnabled,
-		language,
-		onLine,
-		platform,
-		userAgent,
-	} = navigator;
-	onLine = `<i class="text-muted fa fa-circle"></i> Offline`;
-	if (onLine) onLine = `<i class="text-success fa fa-circle"></i> Live`;
-
-	let netStat = `
-	<h2>Browser Info:</h2>
-	<ul>
-		<!--<li>CodeName: <span id="bro-cname">${appCodeName}</span></li>-->
-		<li>Name: <span id="bro-name">${$.browser.name}</span></li>
-		<li>Version: <span id="bro-ver">${$.browser.version}</span></li>
-		<li>Platform: <span id="bro-plat">${platform}</li>
-		<li>Languages: <span id="bro-lang">${language}</span></li>
-		<li>User Agent: <span id="usr-agent">${userAgent}</li>		
-	</ul>
-	<h2>Status:</h2>
-	<ul>
-		<li>Is Online?: <span id="bro-stat">${onLine}</span></li>
-		<li>Cookies Enabled: <span id="bro-cookie">${
-			cookieEnabled ? "Yes" : "No"
-		}</span></li>
-		<li>Javascript Enabled: <span>${
-			navigator.javaEnabled() ? "Yes" : "No"
-		}</span></li>
-	</ul>
-	<h2> Window Info: </h2>
-	<ul>
-		<li>
-			Dimensions: <span id="win-width"></span>px x
-			<span id="win-height"></span>px
-		</li>
-		<li>
-			Screen: <span id="scr-width">${screen.width}</span>px x 
-			<span id="scr-height">${screen.height}</span>px
-		</li>
-		<li>
-			Resolution: <span id="scr-res">${calculateRatio(
-				screen.pixelDepth,
-				screen.colorDepth
-			)}</span> 
-			(<span id="scr-depth">${screen.colorDepth}</span>bit)
-		</li>
-	</ul>
-	`;
-	$("#netstat-container").html(netStat);
-
-	$("#locateme").click(function () {
-		console.log("Locate Me!");
-		container = $("#locationinfo");
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function (position) {
-				$.getJSON("http://gd.geobytes.com/GetCityDetails?callback=?", function (
-					data
-				) {
-					container.html(
-						`<div class="p-2">
-					Latitude: &nbsp;${position.coords.latitude}
-					<br>Longitude: &nbsp;${position.coords.longitude}
-					<br>
-					Remote IP: &nbsp; ${data.geobytesremoteip}
-					<br>
-					Place: &nbsp; ${data.geobytesfqcn}
-					</div>
-					`
-					);
-				});
-			});
-		} else {
-			container.html(
-				`<span class='text-danger p-2'>Geolocation is not supported by this browser.</span>`
-			);
-		}
-	});
-})(jQuery);
-
-$(window).on("load reload resize", function () {
-	$("#win-width").text($(window).width());
-	$("#win-height").text($(window).height());
-	findLocalIp();
-});
-
 const findLocalIp = (logInfo = true) =>
 	new Promise((resolve, reject) => {
 		window.RTCPeerConnection =
@@ -159,3 +69,95 @@ const findLocalIp = (logInfo = true) =>
 			console.log();
 		};
 	});
+
+(($) => {
+	let {
+		appCodeName,
+		appName,
+		appVersion,
+		cookieEnabled,
+		language,
+		onLine,
+		platform,
+		userAgent,
+	} = navigator;
+	onLine = `<i class="text-muted fa fa-circle"></i> Offline`;
+	if (onLine) onLine = `<i class="text-success fa fa-circle"></i> Live`;
+
+	let netStat = `
+	<h2>Browser Info:</h2>
+	<ul>
+		<!--<li>CodeName: <span id="bro-cname">${appCodeName}</span></li>-->
+		<li>Name: <span id="bro-name">${$.browser.name}</span></li>
+		<li>Version: <span id="bro-ver">${$.browser.version}</span></li>
+		<li>Platform: <span id="bro-plat">${platform}</li>
+		<li>Languages: <span id="bro-lang">${language}</span></li>
+		<li>User Agent: <span id="usr-agent">${userAgent}</li>		
+	</ul>
+	<h2>Status:</h2>
+	<ul>
+		<li>Is Online?: <span id="bro-stat">${onLine}</span></li>
+		<li>Cookies Enabled: <span id="bro-cookie">${
+			cookieEnabled ? "Yes" : "No"
+		}</span></li>
+		<li>Javascript Enabled: <span>${
+			navigator.javaEnabled() ? "Yes" : "No"
+		}</span></li>
+	</ul>
+	<h2> Window Info: </h2>
+	<ul>
+		<li>
+			Dimensions: <span id="win-width"></span>px x
+			<span id="win-height"></span>px
+		</li>
+		<li>
+			Screen: <span id="scr-width">${screen.width}</span>px x 
+			<span id="scr-height">${screen.height}</span>px
+		</li>
+		<li>
+			Resolution: <span id="scr-res">${calculateRatio(
+				screen.pixelDepth,
+				screen.colorDepth
+			)}</span> 
+			(<span id="scr-depth">${screen.colorDepth}</span>bit)
+		</li>
+	</ul>
+	`;
+	$("#netstat-container").html(netStat);
+
+	$("#locateme").click(function () {
+		console.log("Locate Me!");
+		container = $("#locationinfo");
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				container.html(
+					`<ul class="py-3" id="location-container">
+				<li>Latitude: &nbsp;${position.coords.latitude}</li>
+				<li>Longitude: &nbsp;${position.coords.longitude}</li>
+				</ul>
+				`
+				);
+				$.getJSON("http://gd.geobytes.com/GetCityDetails?callback=?", function (
+					data
+				) {
+					$("#location-container").append(
+						`<li>
+					Remote IP: &nbsp; ${data.geobytesremoteip}
+					</li><li>
+					Place: &nbsp; ${data.geobytesfqcn} </li>`
+					);
+				});
+			});
+		} else {
+			container.html(
+				`<span class='text-danger p-2'>Geolocation is not supported by this browser.</span>`
+			);
+		}
+	});
+	findLocalIp();
+})(jQuery);
+
+$(window).on("load reload resize", function () {
+	$("#win-width").text($(window).width());
+	$("#win-height").text($(window).height());
+});
